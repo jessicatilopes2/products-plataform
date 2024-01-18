a37256925ae4601c853502a0764fd7ba85a4c293
Jessica Lopes Duarte Sena <jessicatjlopes15@gmail.com>
2024-01-18 13:09:42 -0300

    fix:  script change

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
-import { PrismaModule } from 'prisma';
 
 @Module({
-  imports: [ProductsModule, PrismaModule],
+  imports: [ProductsModule],
   controllers: [],
   providers: [],
 })
