89ba706e3262bc06a37dc1d8f7c659f12efad42b
Jessica Lopes Duarte Sena <jessicatjlopes15@gmail.com>
2024-01-18 15:56:53 -0300

    feat: add module prisma

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
-  imports: [ProductsModule],
+  imports: [ProductsModule, PrismaModule],
   controllers: [],
   providers: [],
 })
